5ca7b16cf9980c17c917af35326312345603de9c
williams manrique <wmanriques@gmail.com>
2012-07-04 09:51:52 -0500

    init

---
+++ Coloracion/src/algorithm/Algorithm.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithm;
+
+/**
+ *
+ * @author williams
+ */
+public class Algorithm {
+    public int[][] coloracion(int[][] graph){
+        int[][] sol = new int[graph.length][graph.length];
+        return sol;
+    }
+}
